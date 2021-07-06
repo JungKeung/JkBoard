8c21e9f95396f83faf3047073f72110e42ea875f
jung <jungkeung0117@gmail.com>
2021-07-06 11:32:37 +0900

    mysql

---
+++ src/main/java/com/example/JkBoard/JkBoardApplication.java
@@ -0,0 +1,14 @@
+package com.example.JkBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkBoardApplication {
+
+	public static void main(String[] args) {
+		System.out.println (1);
+		SpringApplication.run(JkBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JkBoard/JkBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JkBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
